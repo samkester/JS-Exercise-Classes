7b5066f91125cfcbac11cbbebbdbc57170be95a5
Sam Kester <samkester@gmail.com>
2020-07-23 12:41:05 -0600

    added Car constructor

--- index.js
+++ index.js
@@ -76,7 +76,12 @@ class Person {
 */
 
 class Car {
-
+  constructor(model, milesPerGallon){
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
+  }
 }
 
 /*
